661d7d54af4b70451523343718af6b775797ba24
rwedzony <rwedzony@gmail.com>
2021-09-26 21:54:08 +0200

    spring boot initial app

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # spring-boot-circle-cicd-demo
+
 sample spring boot demo for CI/CD integration
---
+++ src/test/java/com/rafsoftrw/springbootcirclecicddemo/SpringBootCircleCicdDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rafsoftrw.springbootcirclecicddemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootCircleCicdDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
